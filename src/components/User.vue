f8107c38db547f0ee277c431872d4e998e2b7d9d
dreamcatcher88888 <luonanshan@yeah.net>
2020-10-12 19:21:53 +0800

    增加表单

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,44 +1,40 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="createTime"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="user"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="logDetail"
-      label="详情">
-    </el-table-column>
-  </el-table>
+<el-form ref="form" :inline="true" :model="form" class="demo-form-inline">
+  <el-form-item label="用户名" >
+    <el-col :span="24">
+    <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+    </el-col>
+  </el-form-item>
+  <el-form-item label="创建时间">
+    <el-row :gutter="20">
+    <el-col :span="12">
+      <el-date-picker type="date" placeholder="选择开始日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+    </el-col>
+    <el-col :span="12">
+      <el-date-picker type="date" placeholder="选择结束日期" v-model="form.date2" style="width: 100%;"></el-date-picker>
+    </el-col>
+    </el-row>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">搜索</el-button>
+  </el-form-item>
+</el-form>
 </template>
-
-
-
- <script>
-    export default{
-            data(){
-                return{
-                    tableData: [],
-                }
-            },
-            created(){
-                 var that=this
-                 this.$axios.get("http://localhost:8081/demo/log/list")
-                      .then(res => {
-                        console.log(res);
-                        this.tableData = res.data.results.list;
-                      })
-                      .catch(err => {
-                        console.log(err);
-                      });
-            }
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          username: '',
+          date1: '',
+          date2: ''
         }
+      }
+    },
+    methods: {
+      onSubmit() {
+        alert(this.$moment(this.$data.form.date1).format('YYYYMMDD'))
+      }
+    }
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import NormailizeCss from 'normalize.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 import axios from 'axios'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$axios = axios
+Vue.prototype.$moment  = moment
 Vue.component('icon', Icon)
 
 
